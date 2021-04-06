81583e2f031d01378e2cdaeb462451b08f07c49a
Fatih Karahan <hseyin97@gmail.com>
2021-04-06 13:03:45 +0300

    implement project

---
+++ src/main/java/com/example/postgresdemo/model/AuditModel.java
@@ -0,0 +1,39 @@
+package com.example.postgresdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updateAt"},
+        allowGetters = true
+)
+public abstract class AuditModel {
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    private Date updateAt;
+
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
---
+++ src/main/java/com/example/postgresdemo/repository/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.example.postgresdemo.repository;
+
+import com.example.postgresdemo.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
